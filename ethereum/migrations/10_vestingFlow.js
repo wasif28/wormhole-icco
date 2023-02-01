4d5915fcc6236df3b46027e70ef77ed15a0a8ef2
wasif28 <wasif28@hotmail.com>
2023-02-01 17:42:34 +0500

    vestingScript completed till init.

--- ethereum/migrations/10_vestingFlow.js
+++ ethereum/migrations/10_vestingFlow.js
@@ -1,24 +1,25 @@
 const Vesting = require("../build/contracts/VestingWallet.json");
 const ConductorImplementation = require("../build/contracts/ConductorImplementation.json");
+const ContributorImplementation = require("../build/contracts/ContributorImplementation.json");
 const TokenImplementation = require("../build/contracts/TokenImplementation.json");
 const ethereumRootPath = `${__dirname}/..`;
 const DeploymentConfig = require(`${ethereumRootPath}/icco_deployment_config.js`);
 const ethers = require("ethers");
+const Web3 = require("web3");
 
-const {
-  hexToUint8Array,
-  uint8ArrayToHex,
-} = require("@certusone/wormhole-sdk");
 const { zeroPad } = require("@ethersproject/bytes");
 
-const { NodeHttpTransport } = require("@improbable-eng/grpc-web-node-http-transport");
+const {
+  NodeHttpTransport,
+} = require("@improbable-eng/grpc-web-node-http-transport");
 
 const {
   parseSequencesFromLogEth,
   getSignedVAAWithRetry,
   getSignedVAA,
   getEmitterAddressEth,
-  uint8ArrayToHex
+  hexToUint8Array,
+  uint8ArrayToHex,
 } = require("@certusone/wormhole-sdk");
 
 const fs = require("fs");
@@ -32,12 +33,14 @@ async function main() {
     _cliffStartTimeInSeconds: nowTime.toString(),
     _cliffPercentage: "50",
     _linearStartTimeInSeconds: (nowTime + 60).toString(),
-    _linearEndTimeInSeconds: (nowTime + 3600).toString()
+    _linearEndTimeInSeconds: (nowTime + 3600).toString(),
   };
   const projectTokenAddress = "0x065fA01fB472ba2FaF8dcf66F3bE89Fd7667D9C0";
   const fujiUSDT = "0x17dC1446ff1fDb6b2093B9E9b057A772fBb07F95";
   const bscUSDT = "0xA9c64Bdbeb7918C4c3F1cc8B83E044D8F5e3203f";
   const tokenAmount = "100000000000000000000"; // 100 tokens 18 decimals
+
+  let enabledContributorsChains = [6, 4];
 
   let file = fs.readFileSync(path.join(__dirname, "deployedAddresses.json"));
   file = JSON.parse(file);
@@ -45,10 +48,9 @@ async function main() {
   ////////////////// DEPLOY VESTING CONTRACTS /////////////////////////
 
   for (const contributor of file.contributor) {
-
-    if(contributor.contributorChain == 6 || contributor.contributorChain == 4){}
-    else{
-        continue;
+    if (enabledContributorsChains.includes(contributor.contributorChain)) {
+    } else {
+      continue;
     }
 
     const config = DeploymentConfig[contributor.contributorNetwork];
@@ -69,7 +71,7 @@ async function main() {
 
     await vestingDeployed.deployed();
 
-    console.log("vestingDeployed on chainId: ",contributor.contributorChain);
+    console.log("vestingDeployed on chainId: ", contributor.contributorChain);
 
     const fp = path.join(__dirname, "vestingAddresses.json");
     let contents = fs.existsSync(fp)
@@ -85,7 +87,9 @@ async function main() {
       verificationScript: `truffle run verify VestingWallet@${vestingDeployed.address} --network=${contributor.contributorNetwork}`,
     };
 
-    let index = contents.Vesting.findIndex((item) => item.network == contributor.contributorNetwork);
+    let index = contents.Vesting.findIndex(
+      (item) => item.network == contributor.contributorNetwork
+    );
     if (index == -1) {
       contents.Vesting.push(VestingDetails);
     } else {
@@ -97,7 +101,9 @@ async function main() {
 
   //////////////////// CREATE SALE USING VESTING CONTRACTS //////////////////////////////
 
-  let vestingFile = fs.readFileSync(path.join(__dirname, "vestingAddresses.json"));
+  let vestingFile = fs.readFileSync(
+    path.join(__dirname, "vestingAddresses.json")
+  );
   vestingFile = JSON.parse(vestingFile);
 
   let vestingFuji;
@@ -106,16 +112,13 @@ async function main() {
   let vestingFantom;
 
   for (const elem of vestingFile.Vesting) {
-    if(elem.chain == 6){
+    if (elem.chain == 6) {
       vestingFuji = elem.contractAddress;
-    }
-    else if(elem.chain == 4){
+    } else if (elem.chain == 4) {
       vestingBsc = elem.contractAddress;
-    }
-    else if(elem.chain == 5){
+    } else if (elem.chain == 5) {
       vestingMumbai = elem.contractAddress;
-    }
-    else if(elem.chain == 10){
+    } else if (elem.chain == 10) {
       vestingFantom = elem.contractAddress;
     }
   }
@@ -170,29 +173,32 @@ async function main() {
     ],
   };
 
-  console.log("Created Sale Data: ",createSaleData);
+  console.log("Created Sale Data: ", createSaleData, "\n");
 
   const conductor = file.conductor;
 
-  const config = DeploymentConfig[conductor.conductorNetwork];
-  const privateKey = config.mnemonic;
-  const rpc = config.rpc;
-  const provider = new ethers.providers.JsonRpcProvider(rpc);
-  const wallet = new ethers.Wallet(privateKey, provider);
+  const conductorConfig = DeploymentConfig[conductor.conductorNetwork];
+  const conductorPrivateKey = conductorConfig.mnemonic;
+  const conductorRpc = conductorConfig.rpc;
+  const conductorProvider = new ethers.providers.JsonRpcProvider(conductorRpc);
+  const conductorWallet = new ethers.Wallet(
+    conductorPrivateKey,
+    conductorProvider
+  );
 
   const conductorContract = new ethers.Contract(
     conductor.conductorAddress,
     ConductorImplementation.abi,
-    wallet
-  )
+    conductorWallet
+  );
   const erc20Contract = new ethers.Contract(
     projectTokenAddress,
     TokenImplementation.abi,
-    wallet
-  )
+    conductorWallet
+  );
 
   let allowance = await erc20Contract.allowance(
-    wallet.address,
+    conductorWallet.address,
     conductor.conductorAddress
   );
   allowance = parseInt(BigInt(allowance).toString());
@@ -203,22 +209,75 @@ async function main() {
     );
 
     await approve.wait();
-    console.log("token approved...");
+    console.log("tokens approved...", "\n");
   }
 
-  const tx = await conductorContract.createSale(
+  const createSaleTx = await conductorContract.createSale(
     createSaleData.Raise,
     createSaleData.Token,
     createSaleData.Vesting
   );
 
-  await tx.wait();
-  console.log("sale created: ", tx);
+  await createSaleTx.wait(conductorConfig.consistencyLevel);
+  console.log("sale created: ", createSaleTx.hash, "\n");
 
   //////////////////// FIND VAA OF CREATED SALE //////////////////////////////
 
+  let web3 = new Web3(conductorRpc);
+  let vaaTx = await web3.eth.getTransactionReceipt(createSaleTx.hash);
 
+  const sequences = parseSequencesFromLogEth(vaaTx, conductorConfig.wormhole);
 
+  let vaaArray = [];
+
+  for (const sequence of sequences) {
+    try {
+      const signedVAA = await getSignedVAA(
+        "https://wormhole-v2-testnet-api.certus.one",
+        6,
+        getEmitterAddressEth(conductor.conductorAddress),
+        sequence,
+        {
+          transport: NodeHttpTransport(),
+        }
+      );
+
+      let vaa = uint8ArrayToHex(signedVAA.vaaBytes);
+      vaaArray.push(`0x${vaa}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log("vaaArray", vaaArray, "\n");
+
+  ///////////////////////////// INIT EACH SALE ON EACH CONTRIBUTOR ////////////////////////////////////////////
+
+  for (const contributor of file.contributor) {
+    if (enabledContributorsChains.includes(contributor.contributorChain)) {
+    } else {
+      continue;
+    }
+
+    const config = DeploymentConfig[contributor.contributorNetwork];
+    const privateKey = config.mnemonic;
+    const rpc = config.rpc;
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const wallet = new ethers.Wallet(privateKey, provider);
+
+    const contributorContract = new ethers.Contract(
+      contributor.contributorAddress,
+      ContributorImplementation.abi,
+      wallet
+    );
+
+    const initSaleTx = await contributorContract.initSale(vaaArray[0]);
+    await initSaleTx.wait();
+    console.log("Contributor Chain Init: ", contributor.contributorChain);
+    console.log("tx hash: ", initSaleTx.hash, "\n");
+
+  }
+  
 }
 
 main();
